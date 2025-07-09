bd59388618da54f06658de95d7fd7b4a21068b5c
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-07-09 15:58:47 +0530

    Add SafeScreen component and implement theming colors

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 import SpeakerTest from "../components/SpeakerTest";
+import { COLORS } from "@/constants/colors";
 
 export default function Index() {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: COLORS.background }}>
       <Text style={{ fontSize: 20, margin: 20 }}>Welcome to the Speaker Test App!</Text>
       <SpeakerTest />
       <Text style={{ fontSize: 16, margin: 20 }}>
