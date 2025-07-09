83eb91ce6f76576b5491f4b3fdfb6dc69a72636c
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-07-09 21:52:04 +0530

    Refactor Index component: replace View with ScrollView and add additional test components

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import SpeakerTest from "../components/SpeakerTest";
+import MicrophoneTest from "../components/MicrophoneTest";
+import BackCameraTest from "../components/BackCameraTest";
+import FlashlightTest from "../components/FlashlightTest";
+import BluetoothTest from "../components/BluetoothTest";
 import { COLORS } from "@/constants/colors";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: COLORS.background }}>
-      <Text style={{ fontSize: 20, margin: 20 }}>Welcome to the Speaker Test App!</Text>
+    <ScrollView style={{ flex: 1, backgroundColor: COLORS.background }}>
+      <Text style={{ fontSize: 24, margin: 20, color: "white" }}>Device Checker</Text>
       <SpeakerTest />
+      <MicrophoneTest />
+      {/* <BackCameraTest /> */}
+      <FlashlightTest />
+      <BluetoothTest />
       <Text style={{ fontSize: 16, margin: 20 }}>
         This app allows you to test your device's speaker functionality.
       </Text>
-    </View>
+    </ScrollView>
   );
 }
